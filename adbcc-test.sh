779312d01549105ec28fcbe9ec56d8a12e8d875f
RekGRpth <rekgrpth@gmail.com>
2023-09-07 12:29:11 +0500

    test

--- adbcc-test.sh
+++ adbcc-test.sh
@@ -2,5 +2,6 @@
 
 (
 cd "$HOME/src/adbcc/adcc-extension"
-make -j"$(nproc)" installcheck
+#make -j"$(nproc)" installcheck
+/home/.local$GP_MAJOR/lib/postgresql/pgxs/src/makefiles/../../src/test/regress/pg_regress --load-extension=plpythonu --load-extension=socket --load-extension=gp_inject_fault cdb_dispatch_replace_ccnt
 ) 2>&1 | tee "$HOME/adbcc-test.log"
