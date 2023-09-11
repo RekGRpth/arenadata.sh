f4ccce0c3274123476bcd2f12234c136d0104321
RekGRpth <rekgrpth@gmail.com>
2023-09-11 15:09:12 +0500

    up

--- gpbackup-test.sh
+++ gpbackup-test.sh
@@ -1,16 +1,18 @@
 #!/bin/sh -eux
 
 (
-cd "$HOME/src/gpdb$GP_MAJOR/contrib/dummy_seclabel"
-make -j"$(nproc)" install
+#cd "$HOME/src/gpdb$GP_MAJOR/contrib/dummy_seclabel"
+#make -j"$(nproc)" install
 cd "$HOME/src/gpbackup"
-gpconfig -c shared_preload_libraries -v dummy_seclabel
-gpstop -afr
+#gpconfig -c shared_preload_libraries -v dummy_seclabel
+#gpstop -afr
 #make -j"$(nproc)" test
 #make -j"$(nproc)" unit
 #make -j"$(nproc)" integration
 #make -j"$(nproc)" end_to_end
-export CUSTOM_BACKUP_DIR="/tmp"
-export GINKGO_FLAGS="-r --keep-going --randomize-suites --randomize-all --no-color --vv"
-ginkgo $GINKGO_FLAGS --timeout=3h --poll-progress-after=0s end_to_end -- --custom_backup_dir $CUSTOM_BACKUP_DIR --ginkgo.focus "restores from --exclude filtered incremental backup with partition tables"
+export CUSTOM_BACKUP_DIR="$HOME/gpbackup"
+export GINKGO_FLAGS="-r --keep-going --randomize-suites --randomize-all --no-color --v"
+#ginkgo $GINKGO_FLAGS --timeout=3h --poll-progress-after=0s end_to_end -- --custom_backup_dir $CUSTOM_BACKUP_DIR --ginkgo.focus "restores from --exclude filtered incremental backup with partition tables"
+#ginkgo $GINKGO_FLAGS --timeout=3h --poll-progress-after=0s end_to_end -- --custom_backup_dir $CUSTOM_BACKUP_DIR --ginkgo.focus "panics if given a leaf partition table and --leaf-partition-data is not set"
+ginkgo $GINKGO_FLAGS --timeout=3h --poll-progress-after=0s end_to_end -- --custom_backup_dir $CUSTOM_BACKUP_DIR --ginkgo.focus "Exclude subpartitions for given root partition in leaf-partition-data mode"
 ) 2>&1 | tee "$HOME/gpbackup-test.log"
