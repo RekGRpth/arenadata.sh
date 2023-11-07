ddd8ae865996d2557ed642296677110f9dc49c09
RekGRpth <rekgrpth@gmail.com>
2023-11-07 08:16:17 +0500

    up

--- pgindent.sh
+++ pgindent.sh
@@ -8,11 +8,12 @@ rm -f "$BINDIR/detab"
 ln "$BINDIR/entab" "$BINDIR/detab"
 cd "$HOME/src/gpdb$GP_MAJOR/src/tools/pgindent"
 cp -f pgindent "$BINDIR"
-rm -r pg_bsd_indent*
+#rm -r pg_bsd_indent*
+rm -r pg_bsd_indent* || echo $?
 wget --no-check-certificate https://ftp.postgresql.org/pub/dev/pg_bsd_indent-1.3.tar.gz
 tar -xf pg_bsd_indent-1.3.tar.gz
 make -j"$(nproc)" -C pg_bsd_indent
 install -s pg_bsd_indent/pg_bsd_indent "$BINDIR"
-rm -r pg_bsd_indent*
+rm -r pg_bsd_indent* || echo $?
 ) 2>&1 | tee "$HOME/pgindent.log"
 
