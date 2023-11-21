eba16ab960991d5c3852227959a57c5fd423ac70
RekGRpth <rekgrpth@gmail.com>
2023-11-21 16:19:49 +0500

    up

--- kafka-adb.sh
+++ kafka-adb.sh
@@ -10,5 +10,6 @@ bin/zookeeper-server-start.sh -daemon config/zookeeper.properties
 #JVMFLAGS=-Dsun.net.spi.nameservice.provider.1=dns,sun 
 #export KAFKA_OPTS=-Dsun.security.krb5.debug=true
 bin/kafka-server-start.sh -daemon config/server.properties
-tail -F logs/server.log
+#tail -F logs/*.log logs/*.out
+tail -F logs/*.log
 ) 2>&1 | tee "$HOME/kafka-adb.log"
---
+++ librdkafka.sh
@@ -0,0 +1,7 @@
+#!/bin/sh -eux
+
+(
+cd "$HOME/src/librdkafka"
+./configure
+make -j"$(nproc)" install
+) 2>&1 | tee "$HOME/librdkafka.log"
