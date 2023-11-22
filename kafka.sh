28d97fabb747f50b6c7e9bf4b0d1488098b0a009
RekGRpth <rekgrpth@gmail.com>
2023-11-22 14:57:43 +0500

    up

--- kafka.sh
+++ kafka.sh
@@ -7,7 +7,8 @@ bin/zookeeper-server-stop.sh || echo $?
 #rm /tmp/kafka-logs/*/* || echo $?
 #rm -rf /tmp/kafka-logs || echo $?
 bin/zookeeper-server-start.sh -daemon config/zookeeper.properties
-KAFKA_OPTS=-Dsun.security.krb5.debug=true bin/kafka-server-start.sh -daemon config/server.properties
+#KAFKA_OPTS=-Dsun.security.krb5.debug=true 
+bin/kafka-server-start.sh -daemon config/server.properties
 #tail -F logs/*.log logs/*.out
 #bin/kafka-topics.sh --create --topic test-topic --bootstrap-server localhost:9093 --command-config config/client.properties
 #bin/kafka-console-producer.sh --topic test-topic --bootstrap-server localhost:9093 --producer.config config/client.properties
