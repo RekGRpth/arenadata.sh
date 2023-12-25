27f260fb5f2c0988f6b71cb3625bb59c33b5d5ce
RekGRpth <rekgrpth@gmail.com>
2023-12-25 10:53:46 +0500

    up

--- behave.sh
+++ behave.sh
@@ -1,17 +1,14 @@
 #!/bin/sh -eux
 
 (
-cd "$HOME/src/gpdb$GP_MAJOR/gpAux/gpperfmon"
-#mkdir build
-#cd build
-#cmake ..
-make -j$(nproc) install
 cd "$HOME/src/gpdb$GP_MAJOR/gpMgmt"
 make -j$(nproc) install
 #source $HOME/qa.sh
 #make -f Makefile.behave behave "$*" TAR=tar 2>&1
 #make -j$(nproc) behave tags=gpexpand
 #make -j$(nproc) behave tags=gplogfilter
-make -j$(nproc) behave tags=gpperfmon
+#make -j$(nproc) behave tags=gpperfmon
+#make -j$(nproc) behave -n "gpperfmon does not lose the query text if its text differs from the text in pg_stat_activity"
 #make -j$(nproc) -f Makefile.behave behave tags=gplogfilter
+behave test/behave/mgmt_utils --tags=gpperfmon -n "gpperfmon does not lose the query text if its text differs from the text in pg_stat_activity"
 ) 2>&1 | tee "$HOME/behave.log"
---
+++ gpperfmon.sh
@@ -0,0 +1,8 @@
+#!/bin/sh -eux
+
+(
+cd "$HOME/src/gpdb$GP_MAJOR/gpAux/gpperfmon"
+make -j$(nproc) install
+gpperfmon_install --port $PGPORT --enable --password password --verbose
+gpstop -afr
+) 2>&1 | tee "$HOME/gpperfmon.log"
