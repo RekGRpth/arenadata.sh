6d6bfeeec7c69464ff786657071f578b27153242
RekGRpth <rekgrpth@gmail.com>
2023-12-27 08:30:58 +0500

    up

--- diskquota.sh
+++ diskquota.sh
@@ -9,7 +9,7 @@ cd build
 cmake .. -DDISKQUOTA_DDL_CHANGE_CHECK=off
 make -j"$(nproc)" install
 #gpconfig -c shared_preload_libraries -v diskquota-2.2
-gpconfig -c shared_preload_libraries -v "$(data/current_binary_name)"
+gpconfig -c shared_preload_libraries -v "$(../tests/data/current_binary_name)"
 createdb diskquota || echo $?
 gpstop -afr
 ) 2>&1 | tee "$HOME/diskquota.log"
