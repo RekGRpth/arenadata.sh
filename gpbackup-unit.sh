741c8114b3158117022cb03315e3d45eb03b9149
RekGRpth <rekgrpth@gmail.com>
2024-01-12 12:50:54 +0500

    up

--- gpbackup-unit.sh
+++ gpbackup-unit.sh
@@ -39,7 +39,7 @@ export GINKGO_FLAGS="-r --keep-going --randomize-suites --randomize-all --no-col
 #ginkgo $GINKGO_FLAGS --timeout=3h --poll-progress-after=0s end_to_end -- --custom_backup_dir $CUSTOM_BACKUP_DIR --ginkgo.focus "runs gpbackup and gprestore with with-stats flag and single-backup-dir"
 #ginkgo $GINKGO_FLAGS --timeout=3h --poll-progress-after=0s integration -- --ginkgo.focus "Tables order cases, when there is a partitioned table to backup"
 #ginkgo $GINKGO_FLAGS --timeout=3h --poll-progress-after=0s integration -- --ginkgo.focus "Wrappers Integration"
-ginkgo $GINKGO_FLAGS --timeout=3h --poll-progress-after=0s integration -- --ginkgo.focus "BackupDataForAllTables"
+#ginkgo $GINKGO_FLAGS --timeout=3h --poll-progress-after=0s integration -- --ginkgo.focus "BackupDataForAllTables"
 #ginkgo $GINKGO_FLAGS --timeout=3h --poll-progress-after=0s integration -- --ginkgo.focus "Tables order cases, when there is a partitioned table to backup" --ginkgo.focus "Tables order cases, when there is an AO/CO table to backup"
 #ginkgo $GINKGO_FLAGS --timeout=3h --poll-progress-after=0s integration -- --ginkgo.focus "Tables order cases, when there is an AO/CO table to backup"
 #ginkgo $GINKGO_FLAGS --timeout=3h --poll-progress-after=0s integration -- --ginkgo.focus "Tables order when no filtering is used or tables filtering is defined"
@@ -51,4 +51,5 @@ ginkgo $GINKGO_FLAGS --timeout=3h --poll-progress-after=0s integration -- --gink
 #ginkgo $GINKGO_FLAGS --timeout=3h --poll-progress-after=0s restore -- --ginkgo.focus "verifies backup file counts match on all segments with resize-cluster"
 #ginkgo $GINKGO_FLAGS --timeout=3h --poll-progress-after=0s restore -- --ginkgo.focus "verifies backup file counts match on all segments with resize-cluster"
 #ginkgo $GINKGO_FLAGS --timeout=3h --poll-progress-after=0s end_to_end -- --custom_backup_dir $CUSTOM_BACKUP_DIR --ginkgo.focus "Exclude subpartitions for given root partition in leaf-partition-data mode" --ginkgo.focus "End to End incremental tests Incremental restore No DDL no partitioning Include/Exclude schemas and tables"
-) 2>&1 | tee "$HOME/gpbackup-test.log"
+ginkgo $GINKGO_FLAGS backup/ filepath/ history/ helper/ options/ report/ restore/ toc/ utils/ testutils/
+) 2>&1 | tee "$HOME/gpbackup-unit.log"
