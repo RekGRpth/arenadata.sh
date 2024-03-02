9cd6d9d79031577aaf12526d14e6fee9a71685bd
RekGRpth <rekgrpth@gmail.com>
2024-03-02 15:56:02 +0500

    up

--- orca.sh
+++ orca.sh
@@ -5,8 +5,10 @@
 cd "$HOME/src/gpdb$GP_MAJOR"
 ln -fs add_libs/gporca orca_src
 cd "$HOME/src/gpdb$GP_MAJOR/add_libs/gporca"
+#make -j"$(nproc)" clean
 #cmake -D CMAKE_INSTALL_PREFIX="$HOME/.local$GP_MAJOR" .
-cmake -D .
+cmake -D CMAKE_INSTALL_PREFIX="$PREFIX/greenplum-db-devel" .
+#cmake -D .
 make -j"$(nproc)" install
 #add_libs/gporca/concourse/build_and_test.py --output_dir=bin_orca --skiptests
 ) 2>&1 | tee "$HOME/orca.log"
