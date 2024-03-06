8c1311c4a69a1285aa1a092810c68ac8690ff2ad
RekGRpth <rekgrpth@gmail.com>
2024-03-06 08:46:01 +0500

    up

--- diskquota-isolation2.sh
+++ diskquota-isolation2.sh
@@ -5,10 +5,12 @@ createdb contrib_regression || echo $?
 cd "$HOME/src/gpdb$GP_MAJOR/src/test/isolation2"
 make -j$(nproc) install
 #cd "$HOME/src/diskquota/tests/isolation2"
+#cd "$HOME/src/diskquota/tests"
 cd "$HOME/src/diskquota/tests"
 ln -fs "../../gpdb$GP_MAJOR/src/test/isolation2/sql_isolation_testcase.py" sql_isolation_testcase.py
 ln -fs "../../gpdb$GP_MAJOR/src/test/isolation2/global_sh_executor.sh" global_sh_executor.sh
-"$HOME/src/gpdb$GP_MAJOR/src/test/isolation2/pg_isolation2_regress" --inputdir=isolation2 --outputdir=isolation2 --init-file=init_file --load-extension=gp_inject_fault config test_create_extension test_dropped_table test_temporary_table test_drop_extension reset_config
+#"$HOME/src/gpdb$GP_MAJOR/src/test/isolation2/pg_isolation2_regress" --inputdir=isolation2 --outputdir=isolation2 --init-file=init_file --load-extension=gp_inject_fault config test_create_extension test_dropped_table test_temporary_table test_drop_extension reset_config
+"$HOME/src/gpdb$GP_MAJOR/src/test/isolation2/pg_isolation2_regress" --inputdir="$HOME/src/diskquota/build/tests/isolation2" --outputdir="$HOME/src/diskquota/build/tests/isolation2" --init-file=init_file --load-extension=gp_inject_fault test_create_extension test_dropped_table test_temporary_table test_drop_extension
 #"$HOME/src/gpdb$GP_MAJOR/src/test/isolation2/pg_isolation2_regress" --inputdir=isolation2 --outputdir=isolation2 --init-file=init_file test_temporary
 #"$HOME/src/gpdb$GP_MAJOR/src/test/isolation2/pg_isolation2_regress" --inputdir=isolation2 --outputdir=isolation2 --init-file=init_file test_dropped
 #"$HOME/src/gpdb$GP_MAJOR/src/test/isolation2/pg_isolation2_regress" --inputdir=isolation2 --outputdir=isolation2 --init-file=init_file --load-extension=gp_inject_fault config test_create_extension test_truncate test_drop_extension reset_config
