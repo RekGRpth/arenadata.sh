bff1830707f38c9196f28b221a4419d1ee9fdd90
RekGRpth <rekgrpth@gmail.com>
2024-03-06 15:35:25 +0500

    up

--- regress.sh
+++ regress.sh
@@ -5,7 +5,7 @@
 cd "$HOME/src/gpdb$GP_MAJOR/src/test/regress"
 make -j$(nproc) clean
 make -j$(nproc) install
-ln -fs "$HOME/src/gpdb$GP_MAJOR/src/test/regress/regress.so" /usr/local/lib/postgresql/regress.so
+ln -fs "$HOME/src/gpdb$GP_MAJOR/src/test/regress/regress.so" "$GPHOME/lib/postgresql/regress.so"
 #./pg_regress --load-extension=gp_inject_fault --init-file=init_file bfv_catalog bfv_olap bfv_statistic bfv_index bfv_partition_plans bfv_aggregate bfv_partition DML_over_joins gporca
 #./pg_regress --load-extension=gp_inject_fault --init-file=init_file appendonly
 #./pg_regress --load-extension=gp_inject_fault --init-file=init_file create_function_1 create_type create_table int8 int4 copy with
@@ -19,7 +19,7 @@ ln -fs "$HOME/src/gpdb$GP_MAJOR/src/test/regress/regress.so" /usr/local/lib/post
 #./pg_regress --load-extension=gp_inject_fault --init-file=init_file bfv_olap olap_setup olap_window_seq
 #./pg_regress --load-extension=gp_inject_fault --init-file=init_file rpt
 #./pg_regress --load-extension=gp_inject_fault --init-file=init_file alter_table_ao alter_table_aocs
-#./pg_regress --load-extension=gp_inject_fault --init-file=init_file scale_factor
+./pg_regress --load-extension=gp_inject_fault --init-file=init_file scale_factor
 #./pg_regress --load-extension=gp_inject_fault --init-file=init_file bfv_planner explain_format gp_recursive_cte gporca rpt scale_factor
 #./pg_regress --load-extension=gp_inject_fault --init-file=init_file create_function_1 create_type create_table int8 int4 copy subselectMY
 #./pg_regress --load-extension=gp_inject_fault --init-file=init_file --schedule=./greenplum_schedule.test
@@ -28,7 +28,7 @@ ln -fs "$HOME/src/gpdb$GP_MAJOR/src/test/regress/regress.so" /usr/local/lib/post
 #./pg_regress --load-extension=gp_inject_fault --init-file=init_file qp_correlated_query
 #./pg_regress --load-extension=gp_inject_fault --init-file=init_file gp_unique_rowid
 #./pg_regress --load-extension=gp_inject_fault --init-file=init_file dispatch
-./pg_regress --load-extension=gp_inject_fault --init-file=init_file tablespace
+#./pg_regress --load-extension=gp_inject_fault --init-file=init_file tablespace
 #./pg_regress --load-extension=gp_inject_fault --init-file=init_file qp_functions
 #./pg_regress --load-extension=gp_inject_fault --init-file=init_file qp_query_execution
 #./pg_regress --load-extension=gp_inject_fault --init-file=init_file qp_bitmapscan
