a963f724751a9f10ebbb83fa74f726489a552667
RekGRpth <rekgrpth@gmail.com>
2024-03-13 09:26:23 +0500

    up

--- config.sh
+++ config.sh
@@ -5,12 +5,15 @@
 cd "$HOME/src/gpdb$GP_MAJOR"
 CONFIGURE_FLAGS=
 if [ "$GP_MAJOR" -eq "5" ]; then
-    sudo rpm -i https://ci.arenadata.io/artifactory/ADB/6.7.1_arenadata4/centos/7/community/x86_64/sigar-1.6.5-163.el7.x86_64.rpm || echo $?
-    sudo rpm -i https://ci.arenadata.io/artifactory/ADB/6.7.1_arenadata4/centos/7/community/x86_64/sigar-headers-1.6.5-163.el7.x86_64.rpm || echo $?
+    sudo rpm -i --replacepkgs https://ci.arenadata.io/artifactory/ADB/6.7.1_arenadata4/centos/7/community/x86_64/sigar-1.6.5-163.el7.x86_64.rpm
+    sudo rpm -i --replacepkgs https://ci.arenadata.io/artifactory/ADB/6.7.1_arenadata4/centos/7/community/x86_64/sigar-headers-1.6.5-163.el7.x86_64.rpm
 #    CONFIGURE_FLAGS="--with-gssapi --enable-debug --enable-depend --enable-cassert --with-libraries=bin_orca/lib --with-includes=bin_orca/include"
 #    CONFIGURE_FLAGS="--with-libraries=$HOME/.local$GP_MAJOR/lib"
     CONFIGURE_FLAGS="--with-libraries=$GPHOME/lib --with-includes=$GPHOME/include"
     export LD_LIBRARY_PATH="$GPHOME/lib"
+elif [ "$GP_MAJOR" -eq "6" ]; then
+    sudo rpm -i --replacepkgs https://downloads.adsw.io/ADB/6.22.0_arenadata38/centos/7/community/x86_64/sigar-1.6.5-1056.git2932df5.el7.x86_64.rpm
+    sudo rpm -i --replacepkgs http://downloads.adsw.io/ADB/6.22.0_arenadata38/centos/7/community/x86_64/sigar-headers-1.6.5-1056.git2932df5.el7.x86_64.rpm
 fi
 #export CONFIGURE_FLAGS="--enable-debug-extensions --with-gssapi --enable-cassert --enable-debug --enable-depend"
 #source "$HOME/src/gpdb$GP_MAJOR/concourse/scripts/common.bash"
--- regress.sh
+++ regress.sh
@@ -20,7 +20,7 @@ ln -fs "$HOME/src/gpdb$GP_MAJOR/src/test/regress/regress.so" "$GPHOME/lib/postgr
 #./pg_regress --load-extension=gp_inject_fault --init-file=init_file rpt
 #./pg_regress --load-extension=gp_inject_fault --init-file=init_file alter_table_ao alter_table_aocs
 #./pg_regress --load-extension=gp_inject_fault --init-file=init_file scale_factor
-./pg_regress --load-extension=gp_inject_fault --init-file=init_file bfv_planner explain_format gp_recursive_cte gporca rpt scale_factor
+#./pg_regress --load-extension=gp_inject_fault --init-file=init_file bfv_planner explain_format gp_recursive_cte gporca rpt scale_factor
 #./pg_regress --load-extension=gp_inject_fault --init-file=init_file create_function_1 create_type create_table int8 int4 copy subselectMY
 #./pg_regress --load-extension=gp_inject_fault --init-file=init_file --schedule=./greenplum_schedule.test
 #./pg_regress --load-extension=gp_inject_fault --init-file=init_file --schedule=./greenplum_schedule.test2
@@ -56,6 +56,6 @@ ln -fs "$HOME/src/gpdb$GP_MAJOR/src/test/regress/regress.so" "$GPHOME/lib/postgr
 #./pg_regress --load-extension=gp_inject_fault --init-file=init_file DML_over_joins
 #./pg_regress --load-extension=gp_inject_fault --init-file=init_file alter_db_set_tablespace
 #./pg_regress --load-extension=gp_inject_fault --init-file=init_file partition
-#./pg_regress --load-extension=gp_inject_fault --init-file=init_file gp_tsrf
+./pg_regress --load-extension=gp_inject_fault --init-file=init_file gp_tsrf
 #make -j$(nproc) installcheck -i
 ) 2>&1 | tee "$HOME/regress.log"
