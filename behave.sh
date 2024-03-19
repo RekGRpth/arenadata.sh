f08c23d641302a2279f87fbcd9dd3398836bbca0
RekGRpth <rekgrpth@gmail.com>
2024-03-19 08:13:18 +0500

    up

--- behave.sh
+++ behave.sh
@@ -1,10 +1,39 @@
 #!/bin/sh -eux
 
 (
+if [ ! -f /home/gpadmin/sqldump/dump.sql ]; then
+    mkdir -p /home/gpadmin/sqldump
+    wget -nv https://rt.adsw.io/artifactory/common/dump.sql.xz -O /home/gpadmin/sqldump/dump.sql.xz
+    xz -d /home/gpadmin/sqldump/dump.sql.xz
+fi
 cd "$HOME/src/gpdb$GP_MAJOR/gpMgmt"
 make -j$(nproc) install
-sudo mkdir -p /data/gpdata/gpexpand
+export DATADIRS=/data/gpdata/gpexpand/data
+export MASTER_DATA_DIRECTORY="$DATADIRS/master/gpseg-1"
+gpstop -a || echo $?
+#sudo rm -rf /data/gpdata
+sudo mkdir -p /data/gpdata
 sudo chown -R $USER:$GROUP /data/gpdata
+#sudo bash -c 'echo "cdw" >/etc/hostname'
+if [ "$(hostname)" != "cdw" ]; then
+    sudo bash -c 'echo "$(hostname -i) cdw" >>/etc/hosts'
+    sudo bash -c 'echo "$(hostname -i) sdw1" >>/etc/hosts'
+    sudo bash -c 'echo "$(hostname -i) sdw2" >>/etc/hosts'
+    sudo bash -c 'echo "$(hostname -i) sdw3" >>/etc/hosts'
+    sudo hostname cdw
+fi
+rm "$HOME/gpAdminLogs/"* || echo $?
+#cd /data/gpdata
+#cd "$HOME"
+#ln -fs "$HOME/src/gpdb$GP_MAJOR/gpMgmt/test" .
+#gpstop -afr
+#createdb --owner="$USER" gptest
+#export MASTER_DATA_DIRECTORY=/data/gpdata/coordinator/gpseg-1
+#cd "$HOME/src/gpdb$GP_MAJOR"
+#sudo groupadd --system docker
+#sudo groupmems -a $USER -g docker
+#export IMAGE=behave
+#docker build -t $IMAGE -f arenadata/Dockerfile .
 #source $HOME/qa.sh
 #make -f Makefile.behave behave "$*" TAR=tar 2>&1
 #make -j$(nproc) behave tags=gpexpand
@@ -12,6 +41,7 @@ sudo chown -R $USER:$GROUP /data/gpdata
 #make -j$(nproc) behave tags=gpperfmon
 #make -j$(nproc) behave -n "gpperfmon does not lose the query text if its text differs from the text in pg_stat_activity"
 #make -j$(nproc) -f Makefile.behave behave tags=gplogfilter
+#make -j$(nproc) -f Makefile.behave behave tags=gpexpand
 #behave test/behave/mgmt_utils --tags=gpperfmon -n "install gpperfmon" -n "run gpperfmon" -n "get info about current queries"
 #behave test/behave/mgmt_utils --tags=gpperfmon -n "install gpperfmon" -n "run gpperfmon" -n "get info about current queries"
 #behave test/behave/mgmt_utils --tags=gpperfmon -n "install gpperfmon" -n "run gpperfmon"
@@ -27,6 +57,15 @@ sudo chown -R $USER:$GROUP /data/gpdata
 #behave test/behave/mgmt_utils --tags=gprecoverseg -n "gprecoverseg should not give warning if pg_basebackup is running for the up segments"
 #behave test/behave/mgmt_utils --tags=gpexpand -n "expand the cluster by adding more segments"
 #behave test/behave/mgmt_utils --tags=gpexpand -n "after resuming a duration interrupted redistribution, tables are restored" -n "after a duration interrupted redistribution, state file on standby matches coordinator" -n "after resuming an end time interrupted redistribution, tables are restored"
-behave test/behave/mgmt_utils --tags=gpstop -n 'gpstop gpstop should not print "Failed to kill processes for segment" when locale is different from English'
+#behave test/behave/mgmt_utils --tags=gpstop -n 'gpstop gpstop should not print "Failed to kill processes for segment" when locale is different from English'
+#behave test/behave/mgmt_utils --tags=gpexpand -n 'gpexpand should skip already expanded/broken tables when redistributing'
+#behave test/behave/mgmt_utils --tags=gpexpand --name='gpexpand should skip already expanded/broken tables when redistributing' --verbose
 #behave test/behave/mgmt_utils --tags=gpperfmon -n "install gpperfmon" -n "run gpperfmon" -n "gpperfmon ignore ALTER TABLE SET DISTRIBUTED BY" -n "gpperfmon does not lose the query text if its text differs from the text in pg_stat_activity"
+#arenadata/scripts/run_behave_tests.bash "gpexpand should skip already expanded/broken tables when redistributing"
+#export PEXPECT_LIB="$GPHOME/bin/lib"
+#export TEST_DIR="$HOME/src/gpdb$GP_MAJOR/gpMgmt"
+#export PYTHONPATH="$PYTHONPATH":"$PEXPECT_LIB":"$TEST_DIR"
+#PYTHONPATH="$PYTHONPATH":"$GPHOME/bin/lib":"$HOME/src/gpdb$GP_MAJOR/gpMgmt" 
+#behave "$HOME/src/gpdb$GP_MAJOR/gpMgmt/test/behave/"* --tags=gpexpand --name='gpexpand should skip already expanded/broken tables when redistributing' --verbose
+behave test/behave/mgmt_utils --tags=gpexpand --name='gpexpand should skip already expanded/broken tables when redistributing' --verbose
 ) 2>&1 | tee "$HOME/behave.log"
--- regress.sh
+++ regress.sh
@@ -62,6 +62,11 @@ ln -fs "$HOME/src/gpdb$GP_MAJOR/src/test/regress/regress.so" "$GPHOME/lib/postgr
 #./pg_regress --load-extension=gp_inject_fault --init-file=init_file update_gp
 #./pg_regress --load-extension=gp_inject_fault --init-file=init_file bfv_dml gangsize gp_unique_rowid gporca partition_pruning qp_subquery update_gp
 #./pg_regress --load-extension=gp_inject_fault --init-file=init_file createdb gp_tablespace_with_faults gp_tablespace temp_tablespaces default_tablespace
-./pg_regress --load-extension=gp_inject_fault --init-file=init_file temp_tablespaces
+#./pg_regress --load-extension=gp_inject_fault --init-file=init_file temp_tablespaces
+./pg_regress --load-extension=gp_inject_fault --init-file=init_file functional_deps
+#./pg_regress --load-extension=gp_inject_fault --init-file=init_file create_function_1 create_type create_table int8 int4 copy bitmapscan bitmapscan_ao join_gp
+#./pg_regress --load-extension=gp_inject_fault --init-file=init_file create_function_1 create_type create_table int8 int4 point polygon circle copy create_misc create_index join_gp
+#./pg_regress --load-extension=gp_inject_fault --init-file=init_file create_function_1 create_type create_table int8 int4 point polygon circle copy create_misc join_gp
+#./pg_regress --load-extension=gp_inject_fault --init-file=init_file create_function_1 create_type create_table int4 copy join_gp
 #make -j$(nproc) installcheck -i
 ) 2>&1 | tee "$HOME/regress.log"
