d2cc14d7c0d745d247d9c4bf7ce24276cfb3e5f0
RekGRpth <rekgrpth@gmail.com>
2024-03-21 13:59:34 +0500

    up

--- recovery.sh
+++ recovery.sh
@@ -1,6 +1,14 @@
 #!/bin/sh -eux
 
 (
-cd "$HOME/src/gpdb$GP_MAJOR/src/test/recovery"
-make -j$(nproc) installcheck -i
+export TESTDIR="$HOME/src/gpdb$GP_MAJOR/src/test/recovery"
+cd "$TESTDIR"
+#make -j$(nproc) installcheck -i
+rm -rf tmp_check
+#PATH="/usr/local/bin:$PATH"
+export top_builddir="$HOME/src/gpdb$GP_MAJOR"
+export PG_REGRESS="$top_builddir/src/test/regress/pg_regress"
+export REGRESS_SHLIB="$top_builddir/src/test/regress/regress.so"
+#prove -I ../../../src/test/perl/ -I .  t/*.pl
+prove -I ../../../src/test/perl/ t/101_non_standby_recovery.pl
 ) 2>&1 | tee "$HOME/recovery.log"
