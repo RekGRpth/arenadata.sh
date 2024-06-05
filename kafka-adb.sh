bf66aa66d9941ce54347e5b0e5bd1d112376041b
RekGRpth <rekgrpth@gmail.com>
2024-06-05 07:54:43 +0500

    up

--- kafka-adb.sh
+++ kafka-adb.sh
@@ -1,6 +1,8 @@
 #!/bin/sh -eux
 
 (
+sudo rpm -i --replacepkgs https://downloads.adsw.io/ADB/6.26.2_arenadata55/centos/7/community/x86_64/librdkafka-1.9.2-2980.el7.x86_64.rpm
+sudo rpm -i --replacepkgs https://downloads.adsw.io/ADB/6.26.2_arenadata55/centos/7/community/x86_64/librdkafka-devel-1.9.2-2980.el7.x86_64.rpm
 cd "$HOME/src/kafka-adb"
 make -j"$(nproc)" clean
 make -j"$(nproc)" install
