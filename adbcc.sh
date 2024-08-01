c122cfbaf48a58c422e9ced8de0c999a70bc314e
RekGRpth <rekgrpth@gmail.com>
2024-08-01 13:15:38 +0500

    up

--- adbcc.sh
+++ adbcc.sh
@@ -11,7 +11,7 @@ make -j"$(nproc)"
 make -j"$(nproc)" install
 gpconfig -c shared_preload_libraries -v 'gpadcc'
 gpconfig -c gp_enable_query_metrics -v on
-psql -c "CREATE EXTENSION IF NOT EXISTS gpadcc"
+psql -d postgres -c "CREATE EXTENSION IF NOT EXISTS gpadcc"
 gpstop -afr
 gpconfig -c adcc.monitor_inner_queries -v on
 gpconfig -c adcc.monitor_utility_inner_queries -v on
