54ec32202392bc86ee04353a99d0bdda260803b6
RekGRpth <rekgrpth@gmail.com>
2024-12-24 09:28:00 +0500

    up

--- adbcc-isolation2.sh
+++ adbcc-isolation2.sh
@@ -1,14 +1,22 @@
 #!/bin/bash -eux
 
 (
+export PGOPTIONS="-c optimizer=off"
+cd "$HOME/src/adbcc/adcc-extension/isolation2"
+rm -f expected
+if [[ "$GP_MAJOR" == "6c" || "$GP_MAJOR" == "6u" ]]; then
+    ln -fs expected6 expected
+elif [[ "$GP_MAJOR" == "7c" || "$GP_MAJOR" == "7u" ]]; then
+    ln -fs expected7 expected
+fi
 cd "$HOME/src/gpdb$GP_MAJOR/src/test/isolation2"
 make -j$(nproc) clean
 make -j$(nproc) install
-if [[ "$GP_MAJOR" == "6c" || "$GP_MAJOR" == "6u" ]]; then
-    ln -fs expected6/ "$HOME/src/adbcc/adcc-extension/isolation2/expected"
-elif [[ "$GP_MAJOR" == "7c" || "$GP_MAJOR" == "7u" ]]; then
-    ln -fs expected7/ "$HOME/src/adbcc/adcc-extension/isolation2/expected"
-fi
+#if [[ "$GP_MAJOR" == "6c" || "$GP_MAJOR" == "6u" ]]; then
+#    ln -fs expected6/ "$HOME/src/adbcc/adcc-extension/isolation2/expected"
+#elif [[ "$GP_MAJOR" == "7c" || "$GP_MAJOR" == "7u" ]]; then
+#    ln -fs expected7/ "$HOME/src/adbcc/adcc-extension/isolation2/expected"
+#fi
 #cd "$HOME/src/adbcc"
 #cd "$HOME/src/adbcc/adcc-extension/isolation2"
 #export ISOLATION2_ROOT="$HOME/src/gpdb$GP_MAJOR/src/test/isolation2"
@@ -18,9 +26,9 @@ fi
 #/usr/local/lib/postgresql/pgxs/src/makefiles/../../src/test/regress/pg_regress --load-extension=plpythonu --load-extension=socket --load-extension=gp_inject_fault cdb_dispatch_replace_ccnt
 #/usr/local/lib/postgresql/pgxs/src/makefiles/../../src/test/regress/pg_regress --load-extension=plpythonu --load-extension=socket --load-extension=gp_inject_fault errors
 #/usr/local/lib/postgresql/pgxs/src/makefiles/../../src/test/regress/pg_regress --load-extension=plpythonu --load-extension=gp_inject_fault errors
-#"$HOME/src/gpdb$GP_MAJOR/src/test/isolation2/pg_isolation2_regress" --load-extension=plpythonu --load-extension=gp_inject_fault --init-file=./init_file_adcc node_metric
+#"$HOME/src/gpdb$GP_MAJOR/src/test/isolation2/pg_isolation2_regress" --load-extension=plpython3u --load-extension=gp_inject_fault --init-file=./init_file_adcc node_metric
 #"$HOME/src/gpdb$GP_MAJOR/src/test/isolation2/pg_isolation2_regress" --load-extension=plpythonu --load-extension=gp_inject_fault --init-file=./init_file_adcc node_metric
 #cd "$HOME/src/gpdb$GP_MAJOR/src/test/isolation2"
-#./pg_isolation2_regress  --init-file="$HOME/src/adbcc/adcc-extension/isolation2/init_file_adcc" --inputdir="$HOME/src/adbcc/adcc-extension/isolation2" --outputdir="$HOME/src/adbcc/adcc-extension/isolation2" --load-extension=gp_inject_fault --load-extension=plpython3u node_metric
-./pg_isolation2_regress  --init-file="$HOME/src/adbcc/adcc-extension/isolation2/init_file_adcc" --inputdir="$HOME/src/adbcc/adcc-extension/isolation2" --outputdir="$HOME/src/adbcc/adcc-extension/isolation2" --load-extension=gp_inject_fault --load-extension=plpython3u spill_snapshot
+./pg_isolation2_regress  --init-file="$HOME/src/adbcc/adcc-extension/isolation2/init_file_adcc" --inputdir="$HOME/src/adbcc/adcc-extension/isolation2" --outputdir="$HOME/src/adbcc/adcc-extension/isolation2" --load-extension=gp_inject_fault --load-extension=plpython3u node_metric
+#./pg_isolation2_regress  --init-file="$HOME/src/adbcc/adcc-extension/isolation2/init_file_adcc" --inputdir="$HOME/src/adbcc/adcc-extension/isolation2" --outputdir="$HOME/src/adbcc/adcc-extension/isolation2" --load-extension=gp_inject_fault --load-extension=plpython3u spill_snapshot
 ) 2>&1 | tee "$HOME/adbcc-isolation2.log"
--- adbcc-regress.sh
+++ adbcc-regress.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -eux
 
 (
-#export PGOPTIONS="-c optimizer=off"
+export PGOPTIONS="-c optimizer=off"
 cd "$HOME/src/adbcc/adcc-extension/regress"
 rm -f expected
 if [[ "$GP_MAJOR" == "6c" || "$GP_MAJOR" == "6u" ]]; then
@@ -9,10 +9,10 @@ if [[ "$GP_MAJOR" == "6c" || "$GP_MAJOR" == "6u" ]]; then
 elif [[ "$GP_MAJOR" == "7c" || "$GP_MAJOR" == "7u" ]]; then
     ln -fs expected7 expected
 fi
-#sudo chmod -R 777 /sys/fs/cgroup/{memory,cpu,cpuset}
-#sudo mkdir -p /sys/fs/cgroup/{memory,cpu,cpuset}/gpdb
-#sudo chmod -R 777 /sys/fs/cgroup/{memory,cpu,cpuset}/gpdb
-#sudo chown -R $USER:$GROUP /sys/fs/cgroup/{memory,cpu,cpuset}/gpdb
+sudo chmod -R 777 /sys/fs/cgroup/{memory,cpu,cpuset}
+sudo mkdir -p /sys/fs/cgroup/{memory,cpu,cpuset}/gpdb
+sudo chmod -R 777 /sys/fs/cgroup/{memory,cpu,cpuset}/gpdb
+sudo chown -R $USER:$GROUP /sys/fs/cgroup/{memory,cpu,cpuset}/gpdb
 #gpconfig -c gp_resource_manager -v group
 #gpconfig -r gp_resource_manager
 #gpstop -afr
@@ -33,6 +33,7 @@ fi
 #"$HOME/src/gpdb$GP_MAJOR/src/test/regress/pg_regress" --load-extension=plpythonu --load-extension=socket create_extension bfv_interconnect cdb_dispatch_ccnt errors errors_resgroup inner_queries misc
 #"$HOME/src/gpdb$GP_MAJOR/src/test/regress/pg_regress" --load-extension=plpython3u --load-extension=socket create_extension socket spill_snapshot
 "$HOME/src/gpdb$GP_MAJOR/src/test/regress/pg_regress" --load-extension=plpython3u --load-extension=socket create_extension spill_snapshot
+#"$HOME/src/gpdb$GP_MAJOR/src/test/regress/pg_regress" --load-extension=plpython3u --load-extension=socket create_extension failure_in_txn failure_in_txn_2 failure_in_txn_gc failure_ssid failure_ssid_gc deparse_context
 #"$HOME/src/gpdb$GP_MAJOR/src/test/regress/pg_regress" --load-extension=plpythonu --load-extension=socket create_extension failure_in_txn failure_in_txn_2 failure_in_txn_resgroup errors errors_resgroup
 #"$HOME/src/gpdb$GP_MAJOR/src/test/regress/pg_regress" --load-extension=plpythonu --load-extension=socket create_extension failure_in_txn failure_in_txn_2 errors
 ) 2>&1 | tee "$HOME/adbcc-regress.log"
