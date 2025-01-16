41a675ff7136f135f4ee0165b4a2e1244bdd3e16
RekGRpth <rekgrpth@gmail.com>
2025-01-16 13:42:45 +0500

    up

--- recovery.sh
+++ recovery.sh
@@ -4,12 +4,14 @@
 export TESTDIR="$HOME/src/gpdb$GP_MAJOR/src/test/recovery"
 cd "$TESTDIR"
 #make -j$(nproc) installcheck -i
-rm -rf tmp_check
+rm -rf tmp_check tmp_check_copy
 export top_builddir="$HOME/src/gpdb$GP_MAJOR"
 export PG_REGRESS="$top_builddir/src/test/regress/pg_regress"
 export REGRESS_SHLIB="$top_builddir/src/test/regress/regress.so"
 #prove -I ../../../src/test/perl/ t/101_non_standby_recovery.pl
 #prove -I ../../../src/test/perl/ t/139_archive_mode_always.pl
+prove --verbose -I ../../../src/test/perl/ t/140_archive_mode_always.pl
+#prove --verbose -I ../../../src/test/perl/ t/005_replay_delay.pl
 #prove -I ../../../src/test/perl/ t/121_streaming_and_archiving.pl
-prove -I ../../../src/test/perl/ t/122_streaming_and_archiving.pl
+#prove --verbose -I ../../../src/test/perl/ t/122_streaming_and_archiving.pl
 ) 2>&1 | tee "$HOME/recovery.log"
