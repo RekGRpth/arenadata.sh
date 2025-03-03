8879658ecaf03280e4d9741cb78f467041d8e75a
RekGRpth <rekgrpth@gmail.com>
2025-03-03 09:51:04 +0500

    up

--- basebackup.sh
+++ basebackup.sh
@@ -3,12 +3,14 @@
 (
 export TESTDIR="$HOME/src/gpdb$GP_MAJOR/src/bin/pg_basebackup"
 cd "$TESTDIR"
+rm -rf tmp_check tmp_check_copy
 make -j$(nproc) installcheck -i
 exit
-rm -rf tmp_check tmp_check_copy
 export top_builddir="$HOME/src/gpdb$GP_MAJOR"
 export PG_REGRESS="$top_builddir/src/test/regress/pg_regress"
 export REGRESS_SHLIB="$top_builddir/src/test/regress/regress.so"
+export TESTDATADIR="$TESTDIR/tmp_check"
+export TESTLOGDIR="$TESTDATADIR/log"
 #prove -I ../../../src/test/perl/ t/101_non_standby_recovery.pl
 #prove --verbose -I ../../../src/test/perl/ t/139_archive_mode_always.pl
 #prove --verbose -I ../../../src/test/perl/ t/140_archive_mode_always.pl
@@ -17,6 +19,7 @@ export REGRESS_SHLIB="$top_builddir/src/test/regress/regress.so"
 #prove --verbose -I ../../../src/test/perl/ t/122_streaming_and_archiving.pl
 #prove --verbose -I ../../../src/test/perl/ t/123_streaming_and_archiving.pl
 #prove --verbose -I ../../../src/test/perl/ t/020_archive_status.pl
-prove --verbose -I ../../../src/test/perl/ t/010_pg_basebackup.pl t/050_check_replication.pl
+#prove --verbose -I ../../../src/test/perl/ t/010_pg_basebackup.pl t/050_check_replication.pl
+prove --verbose -I ../../../src/test/perl/ t/050_check_recovery_backup.pl
 #prove --verbose -I ../../../src/test/perl/ t/123_streaming_and_archiving_with_archive_mode_always.pl
 ) 2>&1 | tee "$HOME/basebackup.log"
