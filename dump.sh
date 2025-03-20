e91dce9aef5a8e941c8a0cc40961f9f82073cf6f
Georgy Shelkovy <rekgrpth@gmail.com>
2025-03-20 11:33:43 +0500

    up

--- dump.sh
+++ dump.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -eux
 
 (
-export TESTDIR="$HOME/src/gpdb$GP_MAJOR/src/test/recovery"
+export TESTDIR="$HOME/src/gpdb$GP_MAJOR/src/bin/pg_dump"
 cd "$TESTDIR"
 rm -rf tmp_check tmp_check_copy
 #make -j$(nproc) installcheck -i
@@ -19,14 +19,9 @@ export TESTLOGDIR="$TESTDATADIR/log"
 #prove --verbose -I ../../../src/test/perl/ t/122_streaming_and_archiving.pl
 #prove --verbose -I ../../../src/test/perl/ t/123_streaming_and_archiving.pl
 #prove --verbose -I ../../../src/test/perl/ t/020_archive_status.pl
-#prove --verbose -I ../../../src/test/perl/ t/044_change_recovery_target.pl
-#prove --verbose -I ../../../src/test/perl/ t/004_timeline_switch.pl
-#prove --verbose -I ../../../src/test/perl/ t/010_logical_decoding_timelines.pl
-#prove --verbose -I ../../../src/test/perl/ t/101_restore_point_and_startup_pause.pl
-#prove --verbose -I ../../../src/test/perl/ t/050_check_recovery_backup.pl
-#prove --verbose -I ../../../src/test/perl/ t/037_invalid_database.pl
-prove --verbose -I ../../../src/test/perl/ t/039_end_of_wal.pl
-#prove --verbose -I ../../../src/test/perl/ t/031_recovery_conflict.pl
-#prove --verbose -I ../../../src/test/perl/ t/013_crash_restart.pl
+#prove --verbose -I ../../../src/test/perl/ t/010_pg_basebackup.pl t/050_check_replication.pl
+prove --verbose -I ../../../src/test/perl/ t/002_pg_dump.pl
+#prove --verbose -I ../../../src/test/perl/ t/020_pg_receivewal.pl
+#prove --verbose -I ../../../src/test/perl/ t/010_pg_basebackup.pl
 #prove --verbose -I ../../../src/test/perl/ t/123_streaming_and_archiving_with_archive_mode_always.pl
-) 2>&1 | tee "$HOME/recovery.log"
+) 2>&1 | tee "$HOME/pg_dump.log"
