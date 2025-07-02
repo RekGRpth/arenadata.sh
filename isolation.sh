672dc406a64c1d1bde751bb3efcc5e016678bdaa
Georgy Shelkovy <rekgrpth@gmail.com>
2025-07-02 21:05:25 +0500

    up

--- isolation.sh
+++ isolation.sh
@@ -4,8 +4,8 @@
 #export PGOPTIONS="-c optimizer=on"
 export PGOPTIONS="-c optimizer=off"
 cd "$HOME/src/gpdb$GP_MAJOR/src/test/isolation"
-#make -j$(nproc) installcheck -i
-#exit
+make -j$(nproc) installcheck -i
+exit
 #make -j$(nproc) clean
 #make -j$(nproc) install
 make -j$(nproc) install pg_isolation_regress gpstringsubs.pl gpdiff.pl
@@ -13,6 +13,7 @@ make -j$(nproc) install pg_isolation_regress gpstringsubs.pl gpdiff.pl
 #./pg_isolation_regress  --init-file=../../../src/test/regress/init_file --init-file=./init_file --load-extension=gp_inject_fault vacuum-full-permissions
 #./pg_isolation_regress  --init-file=../../../src/test/regress/init_file --init-file=./init_file --load-extension=gp_inject_fault async-notify
 #./pg_isolation_regress  --init-file=../../../src/test/regress/init_file --init-file=./init_file --load-extension=gp_inject_fault predicate-lock-hot-tuple deadlock-parallel
-./pg_isolation_regress  --init-file=../../../src/test/regress/init_file --init-file=./init_file --load-extension=gp_inject_fault eval-plan-qual
+./pg_isolation_regress  --init-file=../../../src/test/regress/init_file --init-file=./init_file --load-extension=gp_inject_fault inplace-inval
+#./pg_isolation_regress  --init-file=../../../src/test/regress/init_file --init-file=./init_file --load-extension=gp_inject_fault eval-plan-qual
 #./pg_isolation_regress  --init-file=../../../src/test/regress/init_file --init-file=./init_file --load-extension=gp_inject_fault eval-plan-qual-trigger
 ) 2>&1 | tee "$HOME/isolation.log"
---
+++ rmgrdesc.sh
@@ -0,0 +1,13 @@
+#!/bin/sh -eux
+
+(
+cd "$HOME/src/gpdb$GP_MAJOR/src/backend/access/rmgrdesc"
+export CFLAGS="-O0 -ggdb -g3 -fno-omit-frame-pointer -Wclobbered -DUNITTEST"
+make -C test check
+exit
+cd "$HOME/src/gpdb$GP_MAJOR/src/backend"
+make CFLAGS=-DUNITTEST -s unittest-check
+exit
+cd "$HOME/src/gpdb$GP_MAJOR"
+make GPROOT=/usr/local -j"$(nproc)" -s unittest-check
+) 2>&1 | tee "$HOME/rmgrdesc.log"
