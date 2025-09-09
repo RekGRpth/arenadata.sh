96f00fe0e07426a34575ab681cfcffc88de8fa58
Georgy Shelkovy <rekgrpth@gmail.com>
2025-09-09 08:06:23 +0500

    up

--- arenadata_toolkit.sh
+++ arenadata_toolkit.sh
@@ -1,8 +1,15 @@
-#!/bin/sh -eux
+#!/bin/bash -eux
 
-(
-cd "$HOME/src/gpdb$GP_MAJOR/gpcontrib/arenadata_toolkit"
+exec 2>&1 &> >(tee "$HOME/arenadata_toolkit.log")
+
+#(
+if [[ "$GP_MAJOR" == "7" || "$GP_MAJOR" == "8" ]]; then
+    pushd "$HOME/src/arenadata_toolkit"
+else
+    pushd "$HOME/src/gpdb$GP_MAJOR/gpcontrib/arenadata_toolkit"
+fi
 make -j$(nproc) clean
 make -j$(nproc) install
 make -j$(nproc) installcheck
-) 2>&1 | tee "$HOME/arenadata_toolkit.log"
+#) 2>&1 | tee "$HOME/arenadata_toolkit.log"
+popd
