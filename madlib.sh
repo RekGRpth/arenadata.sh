ba46a3809d9ee86c568dbf6622a5d5a05abda9d7
Georgy Shelkovy <rekgrpth@gmail.com>
2025-09-22 14:44:28 +0500

    up

--- madlib.sh
+++ madlib.sh
@@ -5,19 +5,10 @@ exec 2>&1 &> >(tee "$HOME/madlib$GP_MAJOR.log")
 createdb madlib || echo $?
 
 pushd "$HOME/src/madlib"
-#rm -rf build
-#mkdir -p "build$GP_MAJOR"
-#pushd "build$GP_MAJOR"
-#mkdir -p build
-#pushd build
-#export GREENPLUM_EXECUTABLE=/usr/local/bin
-#cmake ..
-#make -j$(nproc) clean
-#make -j$(nproc)
 ./configure
 make -j$(nproc) install
-#export PGDATABASE=madlib
 pushd build
-./src/bin/madpack -p greenplum -v -c /madlib install
+mkdir -p "$HOME/src/madlib/tmp$GP_MAJOR"
+./src/bin/madpack -d "$HOME/src/madlib/tmp$GP_MAJOR" -l -p greenplum -v -c /madlib install
 popd
 popd
