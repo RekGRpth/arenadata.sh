bf60d744203dc536510e2dd5ad990aad213a48e7
Georgy Shelkovy <rekgrpth@gmail.com>
2025-10-01 10:23:59 +0500

    up

--- madlib-test.sh
+++ madlib-test.sh
@@ -2,18 +2,22 @@
 
 exec 2>&1 &> >(tee "$HOME/madlib$GP_MAJOR.log")
 
+rm -rf /usr/local/madlib/Current/madpack/madpack.py
+
 pushd "$HOME/src/madlib"
+./configure -Wno-dev
 make -j$(nproc) install
 popd
 
 rm -rf "$DATADIRS/madlib"
 mkdir -p "$DATADIRS/madlib"
-#/usr/local/madlib/Current/madpack/madpack.py -p greenplum -v -c /madlib -d "$DATADIRS/madlib" -l dev-check -t pmml/pmml
-#/usr/local/madlib/Current/madpack/madpack.py -p greenplum -v -c /madlib -d "$DATADIRS/madlib" -l dev-check -t pmml/pmml_rf
-#/usr/local/madlib/Current/madpack/madpack.py -p greenplum -v -c /madlib -d "$DATADIRS/madlib" -l dev-check -t pmml/pmml_check_fields
-/usr/local/madlib/Current/madpack/madpack.py -p greenplum -v -c /madlib -d "$DATADIRS/madlib" -l dev-check -t dbscan
+export PATH="$PATH:/usr/local/madlib/bin"
+#madpack -p greenplum -v -c /madlib -d "$DATADIRS/madlib" -l dev-check -t pmml/pmml
+#madpack -p greenplum -v -c /madlib -d "$DATADIRS/madlib" -l dev-check -t pmml/pmml_rf
+#madpack -p greenplum -v -c /madlib -d "$DATADIRS/madlib" -l dev-check -t pmml/pmml_check_fields
+madpack -p greenplum -v -c /madlib -d "$DATADIRS/madlib" -l dev-check -t dbscan
 exit
-#/usr/local/madlib/Current/madpack/madpack.py -p greenplum -v -c /madlib -d "$DATADIRS/madlib" -l install
-/usr/local/madlib/Current/madpack/madpack.py -p greenplum -v -c /madlib -d "$DATADIRS/madlib" -l install-check
-/usr/local/madlib/Current/madpack/madpack.py -p greenplum -v -c /madlib -d "$DATADIRS/madlib" -l dev-check
-/usr/local/madlib/Current/madpack/madpack.py -p greenplum -v -c /madlib -d "$DATADIRS/madlib" -l unit-test
+#madpack -p greenplum -v -c /madlib -d "$DATADIRS/madlib" -l install
+madpack -p greenplum -v -c /madlib -d "$DATADIRS/madlib" -l install-check
+madpack -p greenplum -v -c /madlib -d "$DATADIRS/madlib" -l dev-check
+madpack -p greenplum -v -c /madlib -d "$DATADIRS/madlib" -l unit-test
