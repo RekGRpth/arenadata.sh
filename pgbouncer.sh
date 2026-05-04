0bb851c2c43c6fe720370058032f76adcb681cc0
Georgy Shelkovy <rekgrpth@gmail.com>
2026-05-04 14:05:18 +0500

    fix

--- pgbouncer.sh
+++ pgbouncer.sh
@@ -2,6 +2,8 @@
 
 exec 2>&1 &> >(tee "$HOME/pgbouncer.log")
 
+export DEBIAN_FRONTEND=noninteractive
+sudo apt install ldap-utils slapd
 pushd "$HOME/src/pgbouncer"
 ./autogen.sh
 ./configure --with-ldap --enable-cassert --with-pam
@@ -10,4 +12,17 @@ make -j"$(nproc)" clean
 make -j"$(nproc)" install_pgbouncer
 etc/optscan.sh
 make -j"$(nproc)" -C test check
+pip3 install -r requirements.txt
+#PYTHONIOENCODING=utf8 pytest -n "$(nproc)" -r s
+#export PGOPTIONS="-c gp_role=utility --gp_dbid=1 --gp_contentid=0"
+killall -9 postgres || echo "$?"
+export PGOPTIONS="-c gp_role=utility"
+#PYTHONIOENCODING=utf8 pytest -n "$(nproc)" -r s -k test_ldap_auth
+git apply "$(dirname "$0")/pgbouncer.diff"
+set +e
+PYTHONIOENCODING=utf8 pytest -k test_ldap_auth
+code="$?"
+set -e
+git apply "$(dirname "$0")/pgbouncer-revert.diff"
 popd
+exit "$code"
