34a37b8d3ef21c423a986c0a44d3eb48ed0998a2
Georgy Shelkovy <rekgrpth@gmail.com>
2026-05-07 11:34:04 +0500

    up

--- pgbouncer.sh
+++ pgbouncer.sh
@@ -16,7 +16,7 @@ pip3 install -r requirements.txt
 #PYTHONIOENCODING=utf8 pytest -n "$(nproc)" -r s
 #export PGOPTIONS="-c gp_role=utility --gp_dbid=1 --gp_contentid=0"
 killall -9 postgres || echo "$?"
-export PGOPTIONS="-c gp_role=utility"
+#export PGOPTIONS="-c gp_role=utility"
 #PYTHONIOENCODING=utf8 pytest -n "$(nproc)" -r s -k test_ldap_auth
 git apply "$(dirname "$0")/pgbouncer.diff"
 set +e
